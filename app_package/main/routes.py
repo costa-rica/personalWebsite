62072909fb1e719071dc2919135c51b1dac03dd9
costa-rica <nrodrig1@gmail.com>
2023-01-04 17:14:03 +0100

    updated data storage: website now gets data from a df/.pkl file  - no more db

--- app_package/main/routes.py
+++ app_package/main/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
-from flask import render_template, url_for, redirect, flash, request
+from flask import render_template, url_for, redirect, flash, request, current_app
 import os
 # from app_package.models import SocialPosts, sess
 from sc_models import SocialPosts, sess
 from sqlalchemy import desc
 import logging
 from logging.handlers import RotatingFileHandler
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
@@ -46,23 +47,25 @@ logger_main.addHandler(stream_handler)
 @main.route("/", methods=["GET","POST"])
 def home():
     logger_main.info(f"- in home page: / ")
-    social_posts = sess.query(SocialPosts).order_by(desc(SocialPosts.post_date)).all()
+    # social_posts = sess.query(SocialPosts).order_by(desc(SocialPosts.post_date)).all()
     
-    # social_posts = social_posts[0] + social_posts[14]
+    social_posts_df = pd.read_pickle(os.path.join(current_app.config.get('PROJ_DB_PATH'), 'df_existing.pkl'))
+    social_posts_df['post_date_to_datetime'] = pd.to_datetime(social_posts_df['post_date'])
+    social_posts_list_of_dicts = social_posts_df.sort_values('post_date_to_datetime', ascending=False).to_dict('records')
 
     social_posts_list = []
-    for db_post in social_posts:
+    for social_post in social_posts_list_of_dicts:
         temp_post_dict = {}
-        temp_post_dict['username'] = db_post.username
-        temp_post_dict['title'] = db_post.title
-        temp_post_dict['description'] = db_post.description
-        temp_post_dict['url'] = db_post.url
-        if db_post.social_name == 'Stack Overflow':
+        temp_post_dict['username'] = social_post.get('username')
+        temp_post_dict['title'] = social_post.get('title')
+        temp_post_dict['description'] = social_post.get('description')
+        temp_post_dict['url'] = social_post.get('url')
+        if social_post.get('social_name') == 'Stack Overflow':
             temp_post_dict['social_name'] = "Stackoverflow"
         else:
-            temp_post_dict['social_name'] = db_post.social_name
-        temp_post_dict['social_icon'] = db_post.social_icon
-        temp_post_dict['post_date'] = db_post.post_date
+            temp_post_dict['social_name'] = social_post.get('social_name')
+        temp_post_dict['social_icon'] = social_post.get('social_icon')
+        temp_post_dict['post_date'] = social_post.get('post_date')
 
         social_posts_list.append(temp_post_dict)
 
@@ -110,41 +113,76 @@ def home():
     print(display_post)
     return render_template('home.html', display_post = display_post, div_class=div_class)
 
+
 @main.route('/rest_of_posts', methods=['GET','POST'])
 def rest_of_posts():
 
 
+    social_posts_df = pd.read_pickle(os.path.join(current_app.config.get('PROJ_DB_PATH'), 'df_existing.pkl'))
+    social_posts_df['post_date_to_datetime'] = pd.to_datetime(social_posts_df['post_date'])
+    social_posts_list_of_dicts = social_posts_df.sort_values('post_date_to_datetime', ascending=False).to_dict('records')
 
-
-    social_posts = sess.query(SocialPosts).order_by(desc(SocialPosts.post_date)).all()
+    # social_posts = sess.query(SocialPosts).order_by(desc(SocialPosts.post_date)).all()
     
-    # social_posts = social_posts[0] + social_posts[14]
+    # Create List of lists
 
+    size_of_lists_for_pagination = 10
+    counter = 0
+    page_list = []
     social_posts_list = []
-    for db_post in social_posts:
+    for social_post in social_posts_list_of_dicts:
+
         temp_post_dict = {}
-        temp_post_dict['username'] = db_post.username
-        temp_post_dict['title'] = db_post.title
-        temp_post_dict['description'] = db_post.description
-        temp_post_dict['url'] = db_post.url
-        if db_post.social_name == 'Stack Overflow':
+        temp_post_dict['username'] = social_post.get('username')
+        temp_post_dict['title'] = social_post.get('title')
+        temp_post_dict['description'] = social_post.get('description')
+        temp_post_dict['url'] = social_post.get('url')
+        if social_post.get('social_name') == 'Stack Overflow':
             temp_post_dict['social_name'] = "Stackoverflow"
         else:
-            temp_post_dict['social_name'] = db_post.social_name
-        temp_post_dict['social_icon'] = db_post.social_icon
-        temp_post_dict['post_date'] = db_post.post_date
+            temp_post_dict['social_name'] = social_post.get('social_name')
+        temp_post_dict['social_icon'] = social_post.get('social_icon')
+        temp_post_dict['post_date'] = social_post.get('post_date')
 
         social_posts_list.append(temp_post_dict)
 
-    # social_posts_list_2 = [social_posts_list[0]] + [social_posts_list[13]] + [social_posts_list[14]]
-    display_post = []
-    counter = 0
-    for post in social_posts_list:
-                # if counter == 0:# first social post
-        display_post.append(post)
+        if counter % size_of_lists_for_pagination == 0 and counter > 0:
+            page_list.append(social_posts_list)
+            social_posts_list = []
         counter += 1
 
-    return render_template('rest_of_posts.html', display_post = display_post)
+
+    # Determine paginaiton variables 
+
+
+    pagination_position = int(request.args.get('pagination_position')) - 1
+    print('pagination_position (0 - # of pages -1): ', pagination_position)
+    page_list_displayed = page_list[pagination_position]
+    number_of_pages = len(page_list)
+
+    pagination_position = pagination_position + 1
+
+    # 1 - # number of pages
+    if pagination_position > 1:
+        page_previous = pagination_position - 1
+    else:
+        page_previous = pagination_position
+    
+    if pagination_position == number_of_pages:
+        page_next = pagination_position
+    else:
+        page_next = pagination_position + 1
+
+    pagination_tuple = (page_previous, pagination_position, page_next)
+    pagination_dict = {'first':1, 'previous':page_previous, 'current':pagination_position, 'next': page_next, 'last_page': number_of_pages }
+    print('- pagination_dict -')
+    print(pagination_dict)
+    if request.method == 'POST':
+        formDict = request.forms.to_dict()
+        print('formDict: ', formDict)
+
+    return render_template('rest_of_posts.html', display_post = page_list_displayed, number_of_pages = number_of_pages,
+        pagination_dict = pagination_dict)
 
 
 
